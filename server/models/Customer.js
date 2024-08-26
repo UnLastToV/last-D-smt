e51f0adcce3f65b215056bc25a4c08a5c5509154
UnLasToV <som3470100921435@gmail.com>
2024-08-27 04:28:49 +0700

    lastday

---
+++ server/models/Customer.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const CustomerSchema = new Schema({
+  citizenID: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  licenseID: {
+    type: String,
+    required: true
+  },
+  carType: {
+    type: String,
+    required: true
+  },
+  tel: {
+    type: String,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true
+  },
+  cost: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  breakRule: {
+    type: String,
+    required: true
+  },
+  details: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+module.exports = mongoose.model('Customer', CustomerSchema);
